c140782b71d3f51191b57a662e8f808c308b1759
mohan <gowdamohan221@gmail.com>
2026-07-06 15:01:52 +0530

    new commit

---
+++ varib.py
@@ -0,0 +1,22 @@
+####varibale+datatypes############
+username = "mohan"
+followers = 10000
+verified = True
+rating = 4.5
+
+print(username)
+print(followers)
+print(verified)
+print(rating)
+
+username = input("enter the username:")
+followers = int(input("enter followers count:"))
+verified = input("is acc is verified? True/false:")
+rating = float(input("enter rating:"))
+
+print("username:",username)
+print("followers:",followers)
+print("verified:",verified)
+print("rating:",rating)
+
+
